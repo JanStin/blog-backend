5996a1f143863ce93eb97ad87f494d8421ab978b
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-07-03 13:54:32 +0300

    Created ArticleEntity

---
+++ BlogSite/Domain/Entities/ArticleEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Domain.Entities
+{
+    public class ArticleEntity : BaseEntity
+    {
+        public ArticleEntity(int id, string title, string description, string imagePath, string article, string metaKeywords)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ImagePath = imagePath;
+            Article = article;
+            MetaTitle = title;
+            MetaDescription = description;
+            MetaKeywords = metaKeywords;
+            DateAdded = DateTime.UtcNow;
+        }
+    }
+}
