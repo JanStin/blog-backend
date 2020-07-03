b022ab7369c51eec78d1bb636c583c2ade8ac0cf
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-07-03 13:32:57 +0300

    Added base entity

---
+++ BlogSite/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity() => DateAdded = DateTime.UtcNow;
+
+        [Required]
+        public int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual string Description { get; set; }
+        
+        public virtual string ImagePath { get; set; }
+
+        public virtual string Article { get; set; }
+
+        public virtual string MetaTitle { get; set; }
+
+        public virtual string MetaDescription { get; set; }
+
+        public virtual string MetaKeywords { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
