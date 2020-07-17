10661a873cd1f50fa3226064c13c4e5225b24d6c
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-07-17 22:00:48 +0300

    Added js

---
+++ BlogSite/wwwroot/js/index.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(() => {
+    $('.heroblock__button').click(() => {
+        $('html, body').animate({
+            scrollTop: $(".heroblock").next().offset().top  // класс объекта к которому приезжаем
+        }, 1000);
+    });
+});
