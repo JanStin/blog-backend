c913b5f0723fdd191ac4cc80393c4ac25fe02018
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-07-21 11:01:29 +0300

    Added modification in MySqlConnect. Not test

--- BlogSite/Domain/MySqlConnect.cs
+++ BlogSite/Domain/MySqlConnect.cs
@@ -1,4 +1,7 @@
-﻿namespace BlogSite.Domain
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace BlogSite.Domain
 {
     public class MySqlConnect
     {
@@ -10,6 +13,31 @@
         public string Connect()
         {
             return $"Database= {Database};Datasource={Host};User={User};Password={Password}";
+        }
+
+        public List<object> Query(string table, int id)
+        {
+            var connection = new MySqlConnection(Connect());
+            
+            MySqlCommand query = connection.CreateCommand();
+            query.CommandText = $"SELECT * FROM {table} WHERE id = {id}";
+            
+            connection.Open();
+
+            MySqlDataReader result = query.ExecuteReader();
+            
+            List<object> resultList = new List<object>();
+
+            if (result.Read())
+            {   
+                for(var i = 0; i < result.FieldCount; i++)
+                {
+                    resultList.Add(result.GetValue(i));
+                }
+            }
+            connection.Close();
+
+            return resultList;
         }
     }
 }
