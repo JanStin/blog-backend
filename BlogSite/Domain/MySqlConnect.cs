8bf17b7f44a73bf6ee1b7bcc99d612231874c1fa
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-07-21 09:18:29 +0300

    Added support class for connect MySQL

---
+++ BlogSite/Domain/MySqlConnect.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogSite.Domain
+{
+    public class MySqlConnect
+    {
+        private const string Host = "localhost";
+        private const string Database = "blog";
+        private const string User = "root";
+        private const string Password = "root";
+
+        public string Connect()
+        {
+            return $"Database= {Database};Datasource={Host};User={User};Password={Password}";
+        }
+    }
+}
